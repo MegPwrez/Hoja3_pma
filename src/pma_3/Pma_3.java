ced73f32aefe3c2991c0dd20d66da03c03959493
Gregory Jeronimo <gjeronimo-2016116@kinal.edu.gt>
2026-06-10 17:17:57 -0600

    000 creacion de .gitignore

---
+++ src/pma_3/Pma_3.java
@@ -0,0 +1,19 @@
+
+package pma_3;
+
+/**
+ *
+ * @author Gregory Jeronimo
+ */
+public class Pma_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, soy Gregory.");
+        
+        // TODO code application logic here
+    }
+    
+}
