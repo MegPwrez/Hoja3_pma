5de9a034e92220fca4334392336e5a1d4461be30
Gregory Jeronimo <gjeronimo-2016116@kinal.edu.gt>
2026-06-10 17:45:26 -0600

    Se agrego la clase Texto2

---
+++ src/pma_3/Texto2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pma_3;
+
+/**
+ *
+ * @author Amanda Pérez
+ */
+public class Texto2 {
+    public static void main(String[] args){
+        System.out.println("Hacer push al repositorio remoto en GitHub");
+    }
+    
+}
